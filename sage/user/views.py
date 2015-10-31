34a7cf10db5d0d6d74cbab58aee2794907ec3ea3
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 16:09:40 -0400

    added users view

--- sage/user/views.py
+++ sage/user/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from models import *
+import json
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
+
+
+def all_users(request):
+    users = User.objects.all()
+    return JsonResponse(json.dumps(users))
