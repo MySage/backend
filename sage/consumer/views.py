845b4fbd77e8288e8a0272c2363d43623a11fafe
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 19:31:19 -0400

    no message

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -12,7 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def consume(request):
     app_id = "id=762ab8eb-e560-48a9-8b05-a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2"
-    if request.method == "GET":
+    if request.method == 'GET':
         return HttpResponseNotAllowed
 
     message = str(json.loads(request.body)['message'])
@@ -24,5 +24,31 @@ def consume(request):
 
     intent = response.get('intents')[0].get('intent')
 
+    entities = response.get('entities')
+
+    if intent == 'getWeather':
+        return HttpResponse(weather(entities=entities, latitude=39, longitude=139))
     return HttpResponse(intent)
+
+
+def weather(entities, latitude, longitude):
+
+    api_url = str.format("http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}"
+                         "&appid=88ab028ab3f5325465e618dd3a8e4a17", latitude, longitude)
+
+    response = json.loads(urllib2.urlopen(api_url).read())
+
+    return response.get('weather').get('description')
+
+    # term = ''
+    # for entity in entities:
+    #     if entity.get('type') == "Information":
+    #         term = entity.get('entity')
+    #
+    # if term in response.get('weather').get('main') or term in response.get('weather').get('description'):
+    #     return response.get('weather').get('description')
+    #
+    # if term in response:
+    #
+
 
