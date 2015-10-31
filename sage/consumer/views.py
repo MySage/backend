80f3d7b7440a62d542262b508445d84ae66592d6
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 17:16:33 -0400

    no message

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -15,18 +15,6 @@ def consume(request):
         return HttpResponseNotAllowed
 
     json_data = json.loads(request.body)
-    response = urllib2.urlopen(format("https://api.projectoxford.ai/luis/v1/application?id=762ab8eb-e560-48a9-8b05-"
-                                      "a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2&q=%s",
-                                      json_data['message'])).read()
+    response = urllib2.urlopen("https://api.projectoxford.ai/luis/v1/application?id=762ab8eb-e560-48a9-8b05-a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2&q=rain").read()
     return response
 
-
-def all_users(request):
-    users = User.objects.values('name', 'password', 'phone_number')
-    return JsonResponse(dict(users=list(users)))
-
-
-@csrf_exempt
-def login(request):
-    json_data = json.loads(request.body)
-    return HttpResponse(json_data['password'])
