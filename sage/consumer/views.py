2f9af6f7c126b030c2a6b101ff4bf91ddda2d77c
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 17:24:42 -0400

    no message

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -11,10 +11,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def consume(request):
+    app_id = "id=762ab8eb-e560-48a9-8b05-a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2"
     if request.method == "GET":
         return HttpResponseNotAllowed
 
     json_data = json.loads(request.body)
-    response = urllib2.urlopen("https://api.projectoxford.ai/luis/v1/application?id=762ab8eb-e560-48a9-8b05-a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2&q=rain").read()
+    response = urllib2.urlopen(format("https://api.projectoxford.ai/luis/v1/application?%s&q=rain", app_id)).read()
     return HttpResponse(response)
 
