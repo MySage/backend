acd1447da36e690e83e4cfcc3e31caa4699f400a
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 17:17:45 -0400

    no message

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -16,5 +16,5 @@ def consume(request):
 
     json_data = json.loads(request.body)
     response = urllib2.urlopen("https://api.projectoxford.ai/luis/v1/application?id=762ab8eb-e560-48a9-8b05-a0acd9d5e1fd&subscription-key=9699c87139eb4524977f8cb5078282e2&q=rain").read()
-    return response
+    return HttpResponse(response)
 
