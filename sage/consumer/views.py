9c64436308d733585f04e914233a25dc7e6c80de
Kevin Yang <k46yang@uwaterloo.ca>
2015-10-31 17:27:56 -0400

    no message

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -16,6 +16,8 @@ def consume(request):
         return HttpResponseNotAllowed
 
     json_data = json.loads(request.body)
-    response = urllib2.urlopen(str.format("https://api.projectoxford.ai/luis/v1/application?%s&q=rain", app_id)).read()
+
+    response = urllib2.urlopen(str.format("https://api.projectoxford.ai/luis/v1/application?{}&q={}",
+                                          app_id, json_data['message'])).read()
     return HttpResponse(response)
 
