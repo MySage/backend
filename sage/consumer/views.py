20e27694768628dacfbaa6ffaf7455eea35abe57
Rajul Arora <r9arora@uwaterloo.ca>
2015-10-31 22:02:31 -0400

    fix

--- sage/consumer/views.py
+++ sage/consumer/views.py
@@ -113,7 +113,7 @@ def math(entities):
     api_url = str.format("http://api.wolframalpha.com/v2/query?appid=KYP3UW-35R4EETYA3&input={}&format=image", urllib.quote_plus(math_request))
     xml_response =  urllib2.urlopen(url=api_url).read()
     root = elementTree.fromstring(xml_response)
-    return root.tostring()
+    return root.attrib
 
     # for pod in root.findall('pod'):
     #     return pod
