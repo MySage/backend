e789df892c931e43ff9cca593cceeafccd55b9c5
Kevin Yang <k46yang@uwaterloo.ca>
2015-11-01 09:42:50 -0500

    no message

--- sage/deploy.cmd
+++ sage/deploy.cmd
@@ -115,16 +115,6 @@ IF NOT EXIST "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" (
   echo Found compatible virtual environment.
 )
 
-:: 4. Install packages
-echo Pip install requirements.
-env\scripts\pip install -r requirements.txt
-IF !ERRORLEVEL! NEQ 0 goto error
-
-REM Add additional package installation here
-REM -- Example --
-REM env\scripts\easy_install pytz
-REM IF !ERRORLEVEL! NEQ 0 goto error
-
 :: 5. Copy web.config
 IF EXIST "%DEPLOYMENT_SOURCE%\web.%PYTHON_VER%.config" (
   echo Overwriting web.config with web.%PYTHON_VER%.config
